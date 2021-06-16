b4d3538a9fdc2a7055a3be59aa88cdf0baef815b
Anastasiia Salenko <batsvinova@gmail.com>
2021-06-16 17:07:42 +0300

    Added login page

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
+import classes from './Auth.module.css';
+import Button from '../../components/UI/Button/Button'
 
 export default class Auth extends Component {
+
+  loginHandler = () => {
+
+  }
+
+  registerHandler = () => {
+
+  }
+
+  onSubmitHandler = event => {
+    event.preventDefault()
+  }
+
   render() {
     return(
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div>
+          <h1>Aвторізація</h1>
+          <form onSubmi={this.onSubmitHandler} className={classes.AuthForm}>
+            <input type="text"/>
+            <input type="text"/>
+
+            <Button 
+              type="success" 
+              onClick={this.loginHandler}
+            >
+              Увійти
+            </Button>
+
+            <Button 
+              type="primary" 
+              onClick={this.registerHandler}
+            >
+              Зареєструватися
+            </Button>
+          </form>
+        </div>
       </div>
     )
   }
