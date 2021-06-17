6379dab0c446c7eaac188f00c3e2c3bc82dbff74
Anastasiia Salenko <batsvinova@gmail.com>
2021-06-17 16:24:04 +0300

    Stylization Create quiz page

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react';
+import classes from './QuizCreator.module.css';
+import Button from '../../components/UI/Button/Button';
 
 export default class QuizCreator extends Component {
+
+  submitHandler = (event) => {
+    event.preventDefault()
+  }
+
+  addQuestionHandler = () => {
+
+  }
+
+  createQuestionHandler = () => {
+
+  }
+
   render() {
     return(
-      <div>
-        <h1>Quiz Creator</h1>
+      <div className={classes.QuizCreator}>
+        <div>
+          <h1>Cтворення тесту</h1>
+
+          <form onSubmit={this.submitHandler}>
+            <input type={"text"}/>
+            <hr/>
+            <input type={"text"}/>
+            <input type={"text"}/>
+            <input type={"text"}/>
+            <input type={"text"}/>
+
+            <select></select>
+            <Button
+              type="primory"
+              onClick={this.addQuestionHandler}
+            >
+              Додати питання
+            </Button>
+
+            <Button
+              type="success"
+              onClick={this.createQuestionHandler}
+            >
+              Створити тест
+            </Button>
+          </form>
+        </div>
       </div>
     )
   }
