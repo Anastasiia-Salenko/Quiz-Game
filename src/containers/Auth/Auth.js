e61e94a5ab108e06037b90afc7b88c6f608088d3
Anastasiia Salenko <batsvinova@gmail.com>
2021-06-17 13:43:38 +0300

    Validation form login

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,19 +3,116 @@ import classes from './Auth.module.css';
 import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input';
 
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
 export default class Auth extends Component {
+
+  state = {
+    isFormValid: false,
+    formControls: {
+      email: {
+        value: '',
+        type: 'email',
+        label: 'Email',
+        errorMessage: 'Bведіть коректний email',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true,
+        },
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'Пароль',
+        errorMessage: 'Пароль повиннен бути не меньше 6 сімволів',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6
+        },
+      }
+    }
+  };
 
   loginHandler = () => {
 
-  }
+  };
 
   registerHandler = () => {
 
-  }
+  };
 
   onSubmitHandler = event => {
     event.preventDefault()
+  };
+
+  validateControl(value, validation) {
+    if(!validation) {
+      return true;
+    }
+
+    let isValid = true;
+
+    if (validation.required) {
+      isValid = value.trim() !== '' && isValid
+    }
+
+    if (validation.email) {
+      isValid = validateEmail(value) && isValid
+    }
+
+    if (validation.minLength) {
+      isValid = value.length >= validation.minLength && isValid
+    }
+
+    return isValid;
   }
+
+  onChangeHandler = (event, controlName) => {
+    const formControls = { ...this.state.formControls };
+    const control = { ...formControls[controlName] };
+
+    control.value = event.target.value
+    control.touched = true
+    control.valid = this.validateControl(control.value, control.validation)
+
+    formControls[controlName] = control
+
+    let isFormValid = true;
+
+    Object.keys(formControls).forEach((name) => {
+      isFormValid = formControls[name].valid && isFormValid
+    })
+
+    this.setState({
+      formControls, isFormValid
+    })
+  }
+
+  renderInputs() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName]
+      return (
+        <Input 
+          key={controlName + index}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          label={control.label}
+          shouldValidate={!!control.validation}
+          errorMessage={control.errorMessage}
+          onChange={event => this.onChangeHandler(event, controlName)}
+        />
+      )
+    });
+  };
 
   render() {
     return(
@@ -23,19 +120,13 @@ export default class Auth extends Component {
         <div>
           <h1>Aвторізація</h1>
           <form onSubmit={this.onSubmitHandler} className={classes.AuthForm}>
-            <Input 
-              label="Email"
 
-            />
-
-            <Input 
-              label="Пароль"
-              errorMessage={'test'}
-            />
+            { this.renderInputs() }
 
             <Button 
               type="success" 
               onClick={this.loginHandler}
+              disabled={!this.state.isFormValid}
             >
               Увійти
             </Button>
@@ -43,6 +134,7 @@ export default class Auth extends Component {
             <Button 
               type="primary" 
               onClick={this.registerHandler}
+              disabled={!this.state.isFormValid}
             >
               Зареєструватися
             </Button>
@@ -51,4 +143,4 @@ export default class Auth extends Component {
       </div>
     )
   }
-}
+};
